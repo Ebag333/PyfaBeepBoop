26a13879ee6bf3ace0e8b9afedd0f00e0a2c2c18
Ebag333 <ebagola@gmail.com>
2016-10-25 10:36:33 -0700

    print path

--- bot/messenger.py
+++ bot/messenger.py
@@ -8,6 +8,19 @@ from eos.holder_filter import *
 
 logger = logging.getLogger(__name__)
 
+from os import path, getcwd, chdir
+
+def print_my_path():
+    print('cwd:     {}'.format(getcwd()))
+    print('__file__:{}'.format(__file__))
+    print('abspath: {}'.format(path.abspath(__file__)))
+
+print_my_path()
+
+chdir('..')
+
+print_my_path()
+
 
 class Messenger(object):
     def __init__(self, slack_clients):
